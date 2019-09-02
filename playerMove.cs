6a3e44d305cd0d500cd93bcd54f91687627a7532
lukewoodland <50127832+lukewoodland@users.noreply.github.com>
2019-09-01 17:59:00 -0700

    Add files via upload

---
+++ playerMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerMove : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text WinText;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        WinText.text = "";
+        SetCountText();
+    }
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveH, 0f, moveV);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+        if (count >= 9)
+        {
+            WinText.text = "YOU WIN!!!";
+        }
+    }
+}
